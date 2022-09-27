20feab03e7afd802fd5f8f3981ab70eb6ed52429
Kean <snr4978@hotmail.com>
2022-09-27 14:16:22 +0800

    fix:权限树展开动效

--- src/app/routes/basic/role/menu/menu.component.ts
+++ src/app/routes/basic/role/menu/menu.component.ts
@@ -1,8 +1,9 @@
 import { Component, Inject } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { SelectionModel } from '@angular/cdk/collections';
-import { FlatTreeControl } from '@angular/cdk/tree';
+import { NestedTreeControl } from '@angular/cdk/tree';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { MatTreeFlatDataSource, MatTreeFlattener } from '@angular/material/tree';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
 import { HttpService } from '@app/core/services/http.service';
 import { I18nService } from '@app/core/services/i18n.service';
 import { ToastService } from '@app/core/services/toast.service';
@@ -10,7 +11,14 @@ import { ToastService } from '@app/core/services/toast.service';
 @Component({
   selector: 'app-basic-role-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
+  animations: [
+    trigger('expand', [
+      state('collapsed', style({ height: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('collapsed <=> expanded', animate('200ms cubic-bezier(.4, 0, .2, 1)')),
+    ]),
+  ]
 })
 export class RoleMenuComponent {
 
@@ -22,94 +30,99 @@ export class RoleMenuComponent {
     private _toastService: ToastService,
     private _dialogRef: MatDialogRef<RoleMenuComponent>
   ) {
-    this.ctrl = new FlatTreeControl<any>(node => node.level, node => node.expandable);
-    this.tree = new MatTreeFlatDataSource(this.ctrl, new MatTreeFlattener((node: any, level: number) => {
-      return {
-        id: node.data.id,
-        name: node.data.header,
-        level: level,
-        expandable: node.children && node.children.length
-      }
-    }, node => node.level, node => node.expandable, node => node.children));
-    this.selection = new SelectionModel<number>(true);
+    this._ctrl = new NestedTreeControl<any>(node => node.children);
+    this._tree = new MatTreeNestedDataSource<any>();
+    this._selection = new SelectionModel<number>(true);
     this._httpService.get(`roles/${this._id}/menu`).then((res: any) => {
-      this.tree.data = res.menu.children;
-      this.selection.select(...res.permission);
-      this.ctrl.expandAll();
-      this.loading = false;
+      if (res.menu.children?.length) {
+        const stack = [...res.menu.children];
+        while (stack.length) {
+          const item = stack.pop();
+          if (item.children?.length) {
+            item.children.forEach((child: any) => child.parent = item);
+            stack.push(...item.children);
+          }
+        }
+      }
+      this._selection.select(...res.permission);
+      this._tree.data = res.menu.children;
+      for (let item of this._tree.data) {
+        this._ctrl.expandDescendants(item);
+      }
+      this._loading = false;
     });
   }
 
-  public loading: boolean = true;
-  public ctrl: FlatTreeControl<any>;
-  public tree: MatTreeFlatDataSource<any, any, any>;
-  public selection: SelectionModel<number>;
+  private _loading: boolean = true;
+  private _ctrl: NestedTreeControl<any>;
+  private _tree: MatTreeNestedDataSource<any>;
+  private _selection: SelectionModel<number>;
 
-  //是否是内部节点
-  public internal(_: number, node: any): boolean {
-    return node.expandable;
+  public get loading() {
+    return this._loading;
   }
 
+  public get ctrl() {
+    return this._ctrl;
+  }
+
+  public get tree() {
+    return this._tree;
+  }
+
+  public get selection() {
+    return this._selection;
+  }
+
+  //是否可展开
+  public expandable = (_: number, node: any) =>
+    node.children?.length;
+
   //是否因子节点选中而被选中
-  public selected(node: any, indeterminate: boolean): boolean {
-    if (this.ctrl.dataNodes) {
-      const descendants = this.ctrl.getDescendants(node);
-      let result = descendants.some(item => this.selection.isSelected(item.id));
+  public selected = (node: any, indeterminate: boolean) => {
+    if (this._tree.data) {
+      const descendants = this._ctrl.getDescendants(node);
+      let result = descendants.some(item => this._selection.isSelected(item.data.id));
       if (indeterminate) {
-        result &&= !descendants.every(item => this.selection.isSelected(item.id));
+        result &&= !descendants.every(item => this._selection.isSelected(item.data.id));
       }
       return result;
     }
   }
 
   //选择节点
-  public toggle(node: any): void {
-    this.selection.toggle(node.id);
+  public toggle = (node: any) => {
+    this._selection.toggle(node.data.id);
     //后代
-    const descendants = this.ctrl.getDescendants(node);
+    const descendants = this._ctrl.getDescendants(node);
     if (descendants.length) {
-      if (this.selection.isSelected(node.id)) {
-        this.selection.select(...descendants.map(item => item.id));
+      if (this._selection.isSelected(node.data.id)) {
+        this._selection.select(...descendants.map(item => item.data.id));
       }
       else {
-        this.selection.deselect(...descendants.map(item => item.id));
+        this._selection.deselect(...descendants.map(item => item.data.id));
       }
     }
     //祖先
-    let parent = this.parent(node);
+    let parent = node.parent;
     while (parent) {
-      if (this.selection.isSelected(parent.id)) {
+      if (this._selection.isSelected(parent.data.id)) {
         if (!this.selected(parent, false)) {
-          this.selection.deselect(parent.id);
+          this._selection.deselect(parent.data.id);
         }
       }
       else {
         if (this.selected(parent, false)) {
-          this.selection.select(parent.id);
+          this._selection.select(parent.data.id);
         }
       }
-      parent = this.parent(parent);
+      parent = parent.parent;
     }
-  }
-
-  //父节点
-  private parent(node: any): any {
-    const currentLevel = node.level;
-    if (currentLevel < 1) {
-      return null;
-    }
-    for (let i = this.ctrl.dataNodes.indexOf(node) - 1; i >= 0; i--) {
-      const currentNode = this.ctrl.dataNodes[i];
-      if (currentNode.level < currentLevel) {
-        return currentNode;
-      }
-    }
-    return null;
   }
 
   //保存
-  public async save(): Promise<void> {
-    const res = this._httpService.post(`roles/${this._id}/menu`, this.selection.selected).catch(async err => {
+  public save = async () => {
+    const res = this._httpService.post(`roles/${this._id}/menu`, this._selection.selected).catch(async err => {
       switch (err.status) {
         case 410:
           this._toastService.show(this._i18nService.translate(`routes.basic.role.error.gone.${err.error?.propertyName?.toLowerCase()}`));
@@ -129,5 +142,4 @@ export class RoleMenuComponent {
       this._dialogRef.close({ success: true });
     }
   }
-
 }
